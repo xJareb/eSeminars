59b07e577262c93f806f02eadc62ea07ccb3da1f
Admir Jareb <admir.jareb@edu.fit.ba>
2025-04-27 20:42:50 +0200

    Users pagination, filtering data, and get method changed

--- UI/eseminars_desktop/lib/screens/user_list_screen.dart
+++ UI/eseminars_desktop/lib/screens/user_list_screen.dart
@@ -18,33 +18,68 @@ class UserListScreen extends StatefulWidget {
 class _UserListScreenState extends State<UserListScreen> {
 
   late KorisniciProvider provider;
-  
+
+  int _selectedIndex = 0;
+  int pageSize = 4;
+ 
   @override
   void didChangeDependencies(){
     super.didChangeDependencies();
 
     provider = context.read<KorisniciProvider>();
+
+    _loadData();
+  }
+
+  Future<void> _loadData() async{
+    var filter = {
+      'Page' : _selectedIndex,
+      'PageSize': pageSize
+    };
+    result = await provider.get(filter: filter);
+
+     setState((){         
+     });
+  }
+
+  Future<void> _filterData(String query) async{
+    _selectedIndex = 0;
+    var filter = {
+      'ImePrezimeGTE': query,
+      'Page' : _selectedIndex,
+      'PageSize': pageSize,
+    };
+    result = await provider.get(filter: filter);
+    setState(() {});
   }
 
   SearchResult<Korisnik>? result = null;
+ 
   @override
   Widget build(BuildContext context) {
+
     return MasterScreen("Users", Column(
       children: [
         _buildSearch(),
         SizedBox(height: 55,),
-        _buildResultView()
+        _buildResultView(),
+        _buildPaging()
       ],
     ));
   }
+  TextEditingController _userController = TextEditingController();
 
   Widget _buildSearch(){
     return Row(
       children: [
-        Expanded(child: Text("")),
-        Expanded(child: Row(
+        Expanded(flex: 2,child: Text("")),
+        Expanded(flex:1,child: Row(
           children: [
             Expanded(child: TextField(
+              onChanged: (value) {
+                _filterData(value);
+              },
+              controller: _userController,
               decoration: InputDecoration(
               labelText: "Pretraga korisnika",
               labelStyle: TextStyle(fontSize: 15),
@@ -53,14 +88,6 @@ class _UserListScreenState extends State<UserListScreen> {
                 ),
                 )
                 ),
-            SizedBox(width: 10,),
-            ElevatedButton(onPressed: () async{
-              
-              result = await provider.get();
-              setState((){
-                
-              });
-            }, child: Text("Pretrazi")),
             SizedBox(width: 10,),
             ElevatedButton(onPressed: (){}, child: Text("Dodaj",style: TextStyle(fontSize: 15),))
           ],
@@ -71,21 +98,62 @@ class _UserListScreenState extends State<UserListScreen> {
   }
 
   Widget _buildResultView() {
-  return DataTable(columns: [
-    DataColumn(label: Text("Ime")),
-    DataColumn(label: Text("Prezime")),
-    DataColumn(label: Text("Email")),
-    DataColumn(label: Text("Datum rođenja")),
-    DataColumn(label: Text(""))
-  ], rows: result?.result.map((e) =>
-      DataRow(cells: [
-        DataCell(Text(e.ime ?? "")),
-        DataCell(Text(e.prezime ?? "")),
-        DataCell(Text(e.email ?? "")),
-        DataCell(Text(e.datumRodjenja ?? "")),
-        DataCell(ElevatedButton(child: Text("Obriši"),onPressed: (){},))
-      ])
-  ).toList().cast<DataRow>() ?? [],
+    if(result == null){
+      return Center(child: CircularProgressIndicator(),);
+    }
+  return Expanded(
+    child: SingleChildScrollView(
+      child: DataTable(
+        columns: [
+        DataColumn(label: Text("Ime")),
+        DataColumn(label: Text("Prezime")),
+        DataColumn(label: Text("Email")),
+        DataColumn(label: Text("Datum rođenja")),
+        DataColumn(label: Text(""))
+      ], rows: result?.result.map((e) =>
+          DataRow(cells: [
+            DataCell(Text(e.ime ?? "")),
+            DataCell(Text(e.prezime ?? "")),
+            DataCell(Text(e.email ?? "")),
+            DataCell(Text(e.datumRodjenja ?? "")),
+            DataCell(ElevatedButton(child: Text("Obriši"),onPressed: (){},))
+          ])
+      ).toList().cast<DataRow>() ?? [],
+      ),
+    ),
   );
 }
+  Widget _buildPaging(){
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildPreviousPage(),
+          SizedBox(width: 10),
+          Text((_selectedIndex + 1).toString(),style: TextStyle(fontSize: 16),),
+          SizedBox(width: 10),
+          _buildNextPage(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPreviousPage(){
+    return ElevatedButton(onPressed: _selectedIndex > 0 ? () async{
+            setState(() {
+              _selectedIndex--;
+            });
+            await _loadData();
+          } : null, child: Icon(Icons.navigate_before));
+  }
+  Widget _buildNextPage(){
+    int totalItems = result?.count ?? 0;
+    int totalPages = (totalItems / pageSize).ceil();
+    return ElevatedButton(onPressed: (_selectedIndex + 1) <  totalPages ? () async{
+            setState(() {
+              _selectedIndex ++;
+            });
+            await _loadData();
+          }:null, child: Icon(Icons.navigate_next));
+  }
 }
