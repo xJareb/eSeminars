0a85eb289dbed3810731018086e24255b20149cd
Admir Jareb <admir.jareb@edu.fit.ba>
2025-07-01 22:43:26 +0200

    Added Cors to program.cs

--- eSeminars/eSeminars.API/Program.cs
+++ eSeminars/eSeminars.API/Program.cs
@@ -94,6 +94,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(
+    options => options
+        .SetIsOriginAllowed(x => _ = true)
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+);
 
 app.UseHttpsRedirection();
 
